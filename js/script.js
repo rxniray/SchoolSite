b2a9fe533f7c50337d4198298ded88e2e0c19efd
rxniray <93185886+rxniray@users.noreply.github.com>
2024-02-06 16:45:51 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var burger = document.querySelector('.header__burger');
     var nav = document.querySelector('.nav');
+    var body = document.querySelector('body');
 
     burger.addEventListener('click', function(event) {
         burger.classList.toggle('active');
         nav.classList.toggle('active');
+        body.classList.toggle('lock');
     });
 });
